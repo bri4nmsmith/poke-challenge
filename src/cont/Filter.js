7695dcb541505c656b349e49cc1fd2b06d0ac3cb
unknown <bri4n.m.smith>
2020-06-08 07:36:15 -0400

    fix console error

--- src/cont/Filter.js
+++ src/cont/Filter.js
@@ -2,8 +2,8 @@ import React from "react"
 
 function  Filter(props) {
     const listedListList = props.list
-    const items = listedListList.map(item =>{
-        return  <option value={item}> {item} </option>
+    const items = listedListList.map((item,key) =>{
+        return  <option key={key} value={item}> {item} </option>
     })
     return (
         <div>
